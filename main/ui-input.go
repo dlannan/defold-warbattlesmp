b089554c0a4b73552218bc9e643d8236f4944509
David Lannan <david@fmad.io>
2021-09-26 12:13:36 +0930

    Added gooey menu. Looks ok.

--- main/ui-input.go
+++ main/ui-input.go
@@ -1,18 +1,3 @@
-components {
-  id: "ui-menu"
-  component: "/main/ui-input.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "label"
   type: "label"
