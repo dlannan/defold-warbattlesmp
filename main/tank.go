6eeaa6287f7f12884bcc35c46ff2afaaed6f2dd8
dlannan <dlannan@internode.on.net>
2021-10-17 23:46:37 +1030

    Added camera offset - not great, but will do.

--- main/tank.go
+++ main/tank.go
@@ -13,6 +13,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "tank"
+  component: "/main/tank.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
